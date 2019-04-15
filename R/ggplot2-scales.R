d9358a6a6c1ac594157a478c547a60e0b385ec5d
statnmap <statnmap@users.noreply.github.com>
2019-04-15 08:27:00 +0200

    tests ggplot2 themes

--- R/ggplot2-scales.R
+++ R/ggplot2-scales.R
@@ -6,9 +6,9 @@
 #'
 #' @importFrom ggplot2 scale_color_gradientn
 #' @export
-scale_color_dreal_c <- function(palette = "continuous", reverse = FALSE, ...) {
+scale_color_dreal_c <- function(..., palette = "continuous", reverse = FALSE) {
   pal <- dreal_pal(palette = palette, reverse = reverse)
-  scale_color_gradientn(colours = pal(256), ...)
+  scale_color_gradientn(..., colours = pal(256))
 }
 
 #' Discrete color scale constructor for dreal colors
@@ -25,11 +25,11 @@ scale_color_dreal_c <- function(palette = "continuous", reverse = FALSE, ...) {
 #' ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +
 #'    geom_point(size = 4) +
 #'    scale_color_dreal_d()
-scale_color_dreal_d <- function(palette = "discrete", reverse = FALSE, ...) {
+scale_color_dreal_d <- function(..., palette = "discrete", reverse = FALSE) {
   pal <- unname(dreal_palettes[[palette]])
   if (reverse) pal <- rev(pal)
   # discrete_scale("colour", paste0("dreal_", palette), palette = pal, ...)
-  scale_color_manual(values = pal, ...)
+  scale_color_manual(..., values = pal)
 }
 
 #' Continuous fill scale constructor for dreal colors
@@ -46,9 +46,9 @@ scale_color_dreal_d <- function(palette = "discrete", reverse = FALSE, ...) {
 #' ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Sepal.Length)) +
 #'    geom_point(size = 4) +
 #'    scale_color_dreal_c()
-scale_fill_dreal_c <- function(palette = "continuous", reverse = FALSE, ...) {
+scale_fill_dreal_c <- function(..., palette = "continuous", reverse = FALSE) {
   pal <- dreal_pal(palette = palette, reverse = reverse)
-  scale_fill_gradientn(colours = pal(256), ...)
+  scale_fill_gradientn(..., colours = pal(256))
 }
 
 #' Discrete fill scale constructor for dreal colors
@@ -64,10 +64,10 @@ scale_fill_dreal_c <- function(palette = "continuous", reverse = FALSE, ...) {
 #' ggplot(iris, aes(Sepal.Width, fill = Species)) +
 #'   geom_histogram(size = 4, bins = 20) +
 #'   scale_fill_dreal_d()
-scale_fill_dreal_d <- function(palette = "discrete", reverse = FALSE, ...) {
+scale_fill_dreal_d <- function(..., palette = "discrete", reverse = FALSE) {
   pal <- unname(dreal_palettes[[palette]])
   if (reverse) pal <- rev(pal)
   # discrete_scale("colour", paste0("dreal_", palette), palette = pal, ...)
-  scale_fill_manual(values = pal, ...)
+  scale_fill_manual(..., values = pal)
 }
 
